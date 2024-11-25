dc4250663a93d1702ac028e3b3719906a3412820
Stelios-Ch <chatziveis.stel@gmail.com>
2024-11-25 12:49:04 +0100

    Update compare.cpp

--- set1/1/compare.cpp
+++ set1/1/compare.cpp
@@ -1,11 +1,12 @@
 #include "numbers.ih"
 
 size_t Numbers::compare(Numbers const &rhs) const
-{
+{//will return the first index the 2 objects have different
     for (size_t idx = 0; idx != d_size; ++idx)
     {
         if (d_nums[idx] != rhs[idx])
-            return idx + 1;
+            return idx + 1;    //allows it to be used as bool func
+                        //returning false if they have a different value
     }
 
     return 0;
