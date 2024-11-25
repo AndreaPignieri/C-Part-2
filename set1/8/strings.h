20c5d3fcc2d74172355348c5c3418d08ce664119
BogdanI40 <153098116+BogdanI40@users.noreply.github.com>
2024-11-25 15:21:50 +0100

    Update strings.h

--- set1/8/strings.h
+++ set1/8/strings.h
@@ -4,9 +4,8 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <memory> // For std::shared_ptr
+#include <memory>
 
-// Represents a collection of strings with reference counting and copy-on-write
 class Strings
 {
     class Proxy; // Forward declaration of Proxy
@@ -16,32 +15,28 @@ class Strings
     size_t d_size = 0;                   // Number of strings
 
 public:
-    // Constructors and Destructor
     Strings() = default;
-    Strings(int argc, char **argv);
-    Strings(char **environLike);
-    Strings(Strings const &other);       // Copy constructor
-    Strings(Strings &&tmp) noexcept;    // Move constructor
-    ~Strings();
+    Strings(int argc, char **argv);      // From strings3_1.cc
+    Strings(char **environLike);         // From strings3_2.cc
+    Strings(Strings const &other);       // From strings3_1.cc
+    Strings(Strings &&tmp) noexcept;    // From strings3_2.cc
+    ~Strings();                         // From strings3_3.cc
 
-    Strings &operator=(Strings const &rhs); // Copy assignment
-    Strings &operator=(Strings &&rhs) noexcept; // Move assignment
+    Strings &operator=(Strings const &rhs);     // From strings3_4.cc
+    Strings &operator=(Strings &&rhs) noexcept; // From strings3_5.cc
 
-    // Member functions
-    Strings &operator+=(std::string const &next); // Add string (replaces add)
+    Strings &operator+=(std::string const &next); // From strings3_6.cc
 
-    size_t size() const;
-    void swap(Strings &other);
+    size_t size() const;                        // From strings3_7.cc
+    void swap(Strings &other);                  // From strings3_8.cc
 
-    // Index operators
-    std::string const &operator[](size_t idx) const; // For const Strings
-    Proxy operator[](size_t idx);                    // For non-const Strings
+    std::string const &operator[](size_t idx) const; // From strings3_9.cc
+    Proxy operator[](size_t idx);                    // From strings3_10.cc
 
 private:
-    void cow(); // Copy-on-write logic
+    void cow(); // From strings3_11.cc
 
-    // Helper function for allocation
-    void allocate(size_t size);
+    void allocate(size_t size); // From strings3_12.cc
 };
 
 // Nested Proxy class
@@ -51,11 +46,11 @@ class Strings::Proxy
     size_t d_index;   // Index in the Strings object
 
 public:
-    Proxy(Strings *owner, size_t index);
-    Proxy &operator=(std::string const &value); // Assignment operator
-    operator std::string() const;              // Conversion to string
+    Proxy(Strings *owner, size_t index);  // From strings3_13.cc
+    Proxy &operator=(std::string const &value); // From strings3_14.cc
+    operator std::string() const;              // From strings3_15.cc
 
-    friend std::ostream &operator<<(std::ostream &out, Proxy const &proxy);
+    friend std::ostream &operator<<(std::ostream &out, Proxy const &proxy); // From strings3_16.cc
 };
 
 #endif // INCLUDED_STRINGS_
