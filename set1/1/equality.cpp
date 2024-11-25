ac24e22f8a77a6551b8ff456fdd3700120637018
Stelios-Ch <chatziveis.stel@gmail.com>
2024-11-25 12:50:11 +0100

    Update equality.cpp

--- set1/1/equality.cpp
+++ set1/1/equality.cpp
@@ -2,7 +2,7 @@
 
 bool operator==(Numbers const &lhs, Numbers const &rhs)
 {
-    if (lhs.d_size == rhs.d_size && !lhs.compare(rhs))  
+    if (lhs.d_size == rhs.d_size && !lhs.compare(rhs))  //same in every way
         return 1;
 
     return 0;
