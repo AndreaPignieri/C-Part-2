95ccc0582f22cc962b2bf965b68969a051aea7f8
Stelios-Ch <chatziveis.stel@gmail.com>
2024-11-25 11:53:35 +0100

    Update main.cpp

--- set1/6/main.cpp
+++ set1/6/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     show(Msg::NONE);
-    show(Msg::EMERG);
+    show(Msg::NONE | Msg::EMERG);
     show(Msg::ALERT | Msg::CRIT);
     show(Msg::ALL   & (Msg::ERR | Msg::WARNING));
     show(~Msg::NOTICE);
