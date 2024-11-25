124412ddeaa5aab59a1b4a38115568f6a2d2dfba
Stelios-Ch <chatziveis.stel@gmail.com>
2024-11-25 12:55:49 +0100

    Update operatorComp.cpp

--- set1/1/operatorComp.cpp
+++ set1/1/operatorComp.cpp
@@ -4,8 +4,13 @@ strong_ordering operator<=>(Numbers const &lhs, Numbers const &rhs)
 {
     if (lhs.d_size < rhs.d_size)
         return strong_ordering::less;
+        
     else if (lhs.d_size > rhs.d_size)
         return strong_ordering::greater;
+
+    //idx is defined to indicate the first number that differs
+    //in case there is any. (there is the small adjusting needed
+    //to be made for idx since it's pointing one further (explained in compare)
     else if (size_t idx = lhs.compare(rhs))
         return lhs[idx - 1] < rhs[idx - 1] ? strong_ordering::less :
                lhs[idx - 1] > rhs[idx - 1] ? strong_ordering::greater :
