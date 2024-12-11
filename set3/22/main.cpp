52d900d71b1e9720c9d52cd58f4d498ae8c7d92a
BogdanI40 <153098116+BogdanI40@users.noreply.github.com>
2024-12-11 20:02:05 +0100

    Create main.cpp

---
+++ set3/22/main.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+using namespace std;
+
+// Base class definition
+class Base {
+public:
+    virtual void hello() const {  // Use virtual for polymorphism
+        cout << "Hello from Base!" << "\n";
+    }
+};
+
+// Derived class definition
+class Derived : public Base {
+public:
+    void hello() const override {  // Override the base class method
+        cout << "Hello from Derived!" << "\n";
+    }
+};
+
+// Function taking a Base reference
+void caller(Base &obj) {
+    obj.hello();  // Calls Base::hello or Derived::hello depending on dynamic type
+}
+
+int main() {
+    Derived d;
+    caller(d);  // Pass Derived as Base reference
+    return 0;
+}
