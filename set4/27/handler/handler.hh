8f66205b12910adb30421e4634d13349d2e9c873
Stelios-Ch <chatziveis.stel@gmail.com>
2024-12-19 12:03:56 +0100

    Update handler.hh

--- set4/27/handler/handler.hh
+++ set4/27/handler/handler.hh
@@ -3,11 +3,13 @@
 
 #include "../base/base.hh"
 
-class Handler : public Base
-{
-    public:
+class Handler : private Base
+{ //because of private not even a class inheriting Handler can use Msg
     // The enum class Msg is available for its members but not outside of this class
         Msg message;    
+
+    public:
+        //member functions that can use Msg
 };
 
 #endif
