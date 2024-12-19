29d2f8f49486f3fa3278e326e5315d66e2d7d596
Stelios-Ch <chatziveis.stel@gmail.com>
2024-12-19 11:52:12 +0100

    Create processor.hh

---
+++ set4/27/processor/processor.hh
@@ -0,0 +1,11 @@
+#ifndef PROCESSOR_HH
+#define PROCESSOR_HH
+
+class Processor : public Base
+{
+    public:
+    // The enum class Msg is available for its members but not outside of this class
+    Msg message;
+};
+
+#endif
