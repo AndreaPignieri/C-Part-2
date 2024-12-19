b43ff81694e37dcffc3952524fea9aec77448acb
Stelios-Ch <chatziveis.stel@gmail.com>
2024-12-19 12:02:29 +0100

    Update processor.hh

--- set4/27/processor/processor.hh
+++ set4/27/processor/processor.hh
@@ -3,11 +3,14 @@
 
 #include "../base/base.hh"
 
-class Processor : public Base
-{
-    public:
+class Processor : private Base
+{//because of private not even a class inheriting Processor can use enum
+
     // The enum class Msg is available for its members but not outside of this class
     Msg message;
+
+    public:
+        //processor member functions
 };
 
 #endif
