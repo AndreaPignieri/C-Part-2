a3bc28b95198d447f6c2a0e17396152cf2a6f7a6
Stelios-Ch <chatziveis.stel@gmail.com>
2024-12-19 10:18:21 +0100

    Update factory.cpp

--- set4/26/derived/factory.cpp
+++ set4/26/derived/factory.cpp
@@ -3,9 +3,11 @@
 Base **derivedFactory(size_t size)
 {
     Base **arr = new Base*[size];
+    //a Base pointer pointing to size pointers
 
     for (size_t idx = 0; idx != size; ++idx)
         arr[idx] = new Derived();
+    //which pointers are pointing to Derived objects
     
     return arr;
 }
