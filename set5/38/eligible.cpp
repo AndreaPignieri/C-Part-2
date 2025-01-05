5a726692e71843bd01d7fb7af743e7cf7cda8ccc
Stelios-Ch <chatziveis.stel@gmail.com>
2025-01-05 14:35:53 +0100

    Update eligible.cpp

--- set5/38/eligible.cpp
+++ set5/38/eligible.cpp
@@ -1,7 +1,7 @@
 #include "address.ih"
 
 bool Address::eligible( setItSS const &idx, setItSS const &stop, 
-                        set<pair<string,string>> const &KeySet)
+                        set<pair<string,string>> const &KeySet) const
 {
     return (idx != KeySet.end() && stop != idx);
     //the iterator isnt on the end of set
