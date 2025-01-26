8f9a2286732eeda0fa02c2849cc90af6f5b2fb20
Andrea Pignieri <a.pignieri@rug.student.nl>
2025-01-26 15:48:03 +0100

    Last fixes

--- set8/58/main.cc
+++ set8/58/main.cc
@@ -22,23 +22,25 @@ int main() {
     double rhsT[rhsTRows][rhsTCols];
 
     // Fill the matrices with values
-    for (size_t rowIdx = 0; rowIdx < lhsRows; ++rowIdx) {
-        for (size_t colIdx = 0; colIdx < lhsCols; ++colIdx) {
+    for (size_t rowIdx = 0; rowIdx < lhsRows; ++rowIdx) 
+    {
+        for (size_t colIdx = 0; colIdx < lhsCols; ++colIdx)
             lhs[rowIdx][colIdx] = rowIdx + colIdx + 1;
-        }
     }
 
-    for (size_t rowIdx = 0; rowIdx < rhsTRows; ++rowIdx) {
-        for (size_t colIdx = 0; colIdx < rhsTCols; ++colIdx) {
+    for (size_t rowIdx = 0; rowIdx < rhsTRows; ++rowIdx) 
+    {
+        for (size_t colIdx = 0; colIdx < rhsTCols; ++colIdx)
             rhsT[rowIdx][colIdx] = rowIdx + colIdx + 2;
-        }
     }
 
     future<double> fut[lhsRows][rhsTRows]; // Array to hold futures
 
     // Start tasks in detached threads
-    for (size_t rowIdx = 0; rowIdx < lhsRows; ++rowIdx) {
-        for (size_t colIdx = 0; colIdx < rhsTRows; ++colIdx) {
+    for (size_t rowIdx = 0; rowIdx < lhsRows; ++rowIdx) 
+    {
+        for (size_t colIdx = 0; colIdx < rhsTRows; ++colIdx) 
+        {
             // Create a new packaged_task for each computation
             packaged_task<double(double*, double*, int)> task(innerProduct);
             fut[rowIdx][colIdx] = task.get_future();
@@ -50,9 +52,8 @@ int main() {
 
     // Retrieve and display the results
     for (size_t rowIdx = 0; rowIdx < lhsRows; ++rowIdx) {
-        for (size_t colIdx = 0; colIdx < rhsTRows; ++colIdx) {
+        for (size_t colIdx = 0; colIdx < rhsTRows; ++colIdx) 
             cout << fut[rowIdx][colIdx].get() << " ";
-        }
         cout << "\n";
     }
 }
